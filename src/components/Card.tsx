1eec0e833950181b9f3d50e134c52a4416b37649
Anene Praise <anenepraise1@gmail.com>
2025-06-03 19:14:59 +0100

    feat: implement hover animation for shadow with motion

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,23 +5,42 @@ import classes from "./Card.module.css";
 
 import { motion, type Variants } from "motion/react";
 
-const colorVariants: Variants = {
-  initial: { color: "#111111" },
-  hover: { color: "#f4d04e" },
+const shadowVariant1: Variants = {
+  initial: {
+    boxShadow: "16px 16px 0 0 rgba(0, 0, 0, 0)",
+    transition: { duration: 0.5 },
+  },
+  hover: {
+    boxShadow: "16px 16px 0 0 rgba(0, 0, 0, 1)",
+    transition: { duration: 0.5 },
+  },
+};
+
+const shadowVariant2: Variants = {
+  initial: {
+    boxShadow: "8px 8px 0 0 rgba(0, 0, 0, 1)",
+    transition: { duration: 0.5 },
+  },
+  hover: {
+    boxShadow: "8px 8px 0 0 rgba(0, 0, 0, 0)",
+    transition: { duration: 0.5 },
+  },
 };
 
 export default function Card() {
   return (
     <motion.div
-      variants={colorVariants}
+      className={classes["shadow-container"]}
+      variants={shadowVariant1}
       initial="initial"
       animate="initial"
       whileHover="hover"
-      className={`${classes.container} card-container`}
     >
-      <CardImage />
-      <CardContent />
-      <CardFooter />
+      <motion.div variants={shadowVariant2} className={`${classes.container}`}>
+        <CardImage />
+        <CardContent />
+        <CardFooter />
+      </motion.div>
     </motion.div>
   );
 }
--- src/components/CardContent.tsx
+++ src/components/CardContent.tsx
@@ -3,8 +3,8 @@ import classes from "./CardContent.module.css";
 import { motion, type Variants } from "motion/react";
 
 const colorVariants: Variants = {
-  initial: { color: "#111111" },
-  hover: { color: "#f4d04e" },
+  initial: { color: "#111111", transition: { duration: 0.5 } },
+  hover: { color: "#f4d04e", transition: { duration: 0.5 } },
 };
 
 export default function CardContent() {
